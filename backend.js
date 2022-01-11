6cc568eb23844a7df4ff4e42553a7f735ec724fd
Alrynto <48040209+oogenesa@users.noreply.github.com>
2022-01-11 13:43:50 +0700

    heroku deploy 13

--- backend.js
+++ backend.js
@@ -30,7 +30,16 @@ const corsOptions = {
 //   },
 // };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 // app.use(function (req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
 //   res.header("Access-Control-Allow-Credentials", true);
