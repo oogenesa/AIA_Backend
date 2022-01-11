da49e3ef86e57a00a7b27f6f0bc2ebf3b98c2ace
Alrynto <48040209+oogenesa@users.noreply.github.com>
2022-01-11 12:42:15 +0700

    heroku deploy 7

--- backend.js
+++ backend.js
@@ -11,25 +11,35 @@ const port = 5000;
 //   origin: ["http://localhost:3000", "https://aiaflicker.herokuapp.com/"],
 //   credentials: true,
 // };
-const whitelist = [
-  "http://localhost:3000",
-  "110.137.195.198",
-  "https://aiaflicker.herokuapp.com",
-];
-const corsOptions = {
-  origin: function (origin, callback) {
-    console.log("** Origin of request " + origin);
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      console.log("Origin acceptable");
-      callback(null, true);
-    } else {
-      console.log("Origin rejected");
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
+// const whitelist = [
+//   "http://localhost:3000",
+//   "110.137.195.198",
+//   "https://aiaflicker.herokuapp.com",
+// ];
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     console.log("** Origin of request " + origin);
+//     if (whitelist.indexOf(origin) !== -1 || !origin) {
+//       console.log("Origin acceptable");
+//       callback(null, true);
+//     } else {
+//       console.log("Origin rejected");
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
 
 app.use(Routes);
 app.listen(port, () => {
