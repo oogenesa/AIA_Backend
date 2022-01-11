9a3e58b40c620850fc1b14c2515a28fbdbc44e53
Alrynto <48040209+oogenesa@users.noreply.github.com>
2022-01-11 14:22:01 +0700

    heroku deploy 14

--- backend.js
+++ backend.js
@@ -7,10 +7,10 @@ const app = express();
 
 const port = 5000;
 
-const corsOptions = {
-  origin: "https://aiaflicker.herokuapp.com",
-  credentials: true,
-};
+// const corsOptions = {
+//   origin: "https://aiaflicker.herokuapp.com",
+//   credentials: true,
+// };
 
 // const whitelist = [
 //   "http://localhost:3000",
@@ -31,25 +31,6 @@ const corsOptions = {
 // };
 
 // app.use(cors(corsOptions));
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Credentials", true);
-//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
-//   );
-//   next();
-// });
 
 app.use(Routes);
 app.listen(port, () => {
